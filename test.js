2e6dec20d787c46f939c04389e0383ee9021af36
pingping-jiao <77112416+pingping-jiao@users.noreply.github.com>
2026-02-24 11:31:54 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
-function dynamicallyLoadScript(url) { const script = document.createElement("script"); script.src = "https://js-cdn.dynatrace.com/jstag/15c157a40ab/bf95489cqg/309d5a69f212598f_complete.js"; script.async = true; // load asynchronously document.head.appendChild(script); }
+function dynamicallyLoadScript(url) { 
+  const script = document.createElement("script"); 
+  script.src = url; 
+  script.async = true; 
+  // load asynchronously 
+  document.head.appendChild(script); 
+}
+
+
+dynamicallyLoadScript("https://js-cdn.dynatrace.com/jstag/15c157a40ab/bf95489cqg/309d5a69f212598f_complete.js");
